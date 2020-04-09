830c047fe563a741546b269e234e5dc3d4fa0e16
jihye.an <jihye.an@voithru.com>
2020-04-10 08:29:26 +0900

    feat: create routes, views, path

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-// const http = require('http');
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('in the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(shopRoutes);
+app.use('/admin', adminRoutes);
 
 app.use((req, res, next) => {
-    console.log('in another middleware!');
-    res.send('<h2>hello from express!</h2>')
-});
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
 
-// const server = http.createServer(app);
-// server.listen(3000);
 app.listen(3000);
