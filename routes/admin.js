375deea7d6d6600ddcdbc827c12e2424415dc9e2
jihye.an <jihye.an@voithru.com>
2020-04-12 11:07:22 +0900

    feat: add pug template engine

--- routes/admin.js
+++ routes/admin.js
@@ -6,16 +6,19 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
-// /admin/product => POST
-router.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
 
-module.exports = router;
+// module.exports = router;
+exports.routes = router;
+exports.products = products;
